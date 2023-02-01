a1fc27dbf1e60cf91f6efecb26e6347e0f744620
Dionatan <dionatan.1751@gmail.com>
2023-02-01 18:00:24 -0300

    adminjs

---
+++ src/resources/UsersResource.js
@@ -0,0 +1,58 @@
+import AdminJS from 'adminjs'
+
+import User from '../models/user'
+
+export default {
+    resource: User,
+    options: {
+        
+        properties: {
+            id: {
+                position: 1,
+            },
+            initials: {
+                position: 2,
+                isVisible: { list: true, filter: false, show: true, edit: false },
+            },
+            name: {
+                position: 3,
+                isRequired: true,
+            },
+            email: {
+                position: 4,
+                isRequired: true,
+            },
+            role: {
+                position: 5,
+                isRequired: true,
+                availableValues: [
+                    { value: 'admin', label: 'Administrador'},
+                    { value: 'manager', label: 'Gerente'},
+                    { value: 'developer', label: 'Desenvolvedor'},
+                ]
+            },
+            status: {
+                position: 6,
+                isRequired: true,
+                availableValues: [
+                    { value: 'active', label: 'Ativo'},
+                    { value: 'archived', label: 'Arquivado'}                    
+                ]
+            },
+            created_at: {
+                position: 7,
+                isVisible: { list: false, filter: true, show: false, edit: false }
+            },
+            updated_at: {
+                position: 8,
+                isVisible: { list: false, filter: true, show: false, edit: false }
+            },            
+            password: {
+                isVisible: false
+            },
+            password_hash: {
+                isVisible: false
+            }
+        }
+    }
+}
---
+++ src/server.js
@@ -0,0 +1,27 @@
+import "dotenv/config"
+
+import "./database"
+
+import AdminJS from 'adminjs'
+import AdminJSExpress from '@adminjs/express'
+import AdminJSSequelize from "@adminjs/sequelize"
+import express from 'express'
+
+import UsersResource from "./resources/UsersResource" 
+
+AdminJS.registerAdapter(AdminJSSequelize)
+
+const app = express()
+
+const adminJS = new AdminJS({
+    databases: [],
+    rootPath: '/admin',
+    resources: [UsersResource]
+})
+
+const router = AdminJSExpress.buildRouter(adminJS)
+
+app.use(adminJS.options.rootPath, router)
+app.listen(8080, () => {
+    console.log('AdminJS is under http://localhost:8080/admin')
+})
